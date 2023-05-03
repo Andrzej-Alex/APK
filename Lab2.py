7b5d026864804581a0c38a2ace2ed23bc07d98a8
Andrzej-Alex <79633226+Andrzej-Alex@users.noreply.github.com>
2023-05-03 18:34:22 +0200

    Laboratorium 2 Zadanie 1

--- Lab2.py
+++ Lab2.py
@@ -1,3 +1,21 @@
 """
 Andrzej Aleksandrowicz
 """
+
+# 1)
+
+while True:
+    x = int(input("Enter first number: "))
+    y = int(input("Enter second number: "))
+    if x == 0 or y == 0:
+        print("Program stopped working because either x or y is 0")
+
+        break
+
+    product = x * y
+
+    print(f"{x} * {y} = {product}")
+    print("Do you want to continue? (y/n)")
+    answer = input()
+    if answer == "n":
+        print("Program stopped working")
