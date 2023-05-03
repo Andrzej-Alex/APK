f1c77727ca6e0b53b0de74a635dc1d9f4c2b7a82
Andrzej-Alex <79633226+Andrzej-Alex@users.noreply.github.com>
2023-05-03 18:49:14 +0200

    Laboratorium 2 Zadanie 1

--- Lab2.py
+++ Lab2.py
@@ -1,19 +1,16 @@
 """
-Andrzej Aleksandrowicz
+Andrzej Aleksandrowicz4
 """
 
 # 1)
 
 while True:
-    x = int(input("Enter first number: "))
-    y = int(input("Enter second number: "))
+    x = int(input("Wprowadź pierwszą liczbę: "))
+    y = int(input("Wprowadź drugą liczbę: "))
     if x == 0 or y == 0:
-        print("Program stopped working because either x or y is 0")
-
+        print("Zatrzymanie programu z powodu: x lub y jest ZEREM")
         break
-
     product = x * y
-
     print(f"{x} * {y} = {product}")
     print("Do you want to continue? (y/n)")
     answer = input()
