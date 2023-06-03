25407ecd599a8c590d1d4785ac442a1b69441838
Andrzej-Alex <79633226+Andrzej-Alex@users.noreply.github.com>
2023-06-03 13:42:01 +0200

    Laboratorium

---
+++ Lab6.py
@@ -0,0 +1,30 @@
+# from tkinter import *
+# root = Tk()  # obiekt okno
+# root.configure(background='red') # kolor tła
+# root.configure(cursor='circle') # kursor
+# root.configure(bd=20) # krawędœ
+# root.configure(relief='sunken') # efekt
+# root.title('OOOkkknnoo') # tytuł okna
+# root.geometry('450x200') # rozmiar okna
+# root.mainloop()
+
+'''
+root.resizable(0,0) # nie moliwe zmiany rozmiaru okna
+root.minsize(100,100) # minimalna wielkość okna
+'''
+
+# import a as a
+# # import b as b
+# #
+# # a.a()
+# # b.b()
+#
+# class A(object):
+#
+#  def _init_(self, b, c):
+#  self.y = b
+#  self.z = c
+# class B(A):
+#  def _init_(self, a, *args, **kwargs):
+#  super(B, self)._init_(*args, **kwargs)
+#  self.x = a
